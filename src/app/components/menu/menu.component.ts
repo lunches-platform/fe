474a9391d4aa0dc207a99b9d536b013d2d1b78c6
Yuriy Buchchenko <dezoxel@gmail.com>
2016-07-08 16:30:38 +0300

    LPFE-6: Show CTA text when nothing checked. Disable order buttons. (#24)

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -3,6 +3,7 @@ import {Order, OrderService} from '../../models/order.service';
 import {OrderForm, OrderFormService} from './order.form';
 import {LineItem, LineItemService} from '../line-item/line-item.service';
 import {cloneDeep, filter} from 'lodash';
+import {IScope} from 'angular';
 
 interface ITriggerOrderChangeEvent {
   (arg: { order: Order }): void;
@@ -18,10 +19,12 @@ export class MenuController {
 
   // internal bindings
   orderForm: OrderForm;
+  price: number;
 
   private lineItemsAddedToBasket = false;
 
   constructor(
+    private $scope: IScope,
     private lOrderService: OrderService,
     private lOrderFormService: OrderFormService,
     private lLineItemService: LineItemService
@@ -30,6 +33,7 @@ export class MenuController {
 
     this.initOrder();
     this.initOrderForm();
+    this.initPrice();
   }
 
   calcPrice() {
@@ -72,6 +76,14 @@ export class MenuController {
     this.orderForm = this.lOrderFormService.addItems(this.menu.products.map(product => {
       return new LineItem(product, this.menu.date);
     }), this.orderForm);
+  }
+
+  private initPrice() {
+    this.$scope.$watch(() => this.orderForm, this.updatePrice.bind(this));
+  }
+
+  private updatePrice() {
+    this.price = this.calcPrice();
   }
 }
 
