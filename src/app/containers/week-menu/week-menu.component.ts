5b1d2a530f3a622292c5a9c60b177f7276f708c7
dezoxel <dezoxel@gmail.com>
2016-07-21 15:25:23 +0300

    LPFE-20: Added loading placeholder when week-menu is loading from api

--- src/app/containers/week-menu/week-menu.component.ts
+++ src/app/containers/week-menu/week-menu.component.ts
@@ -1,33 +1,39 @@
+import {ILogService, IQService} from 'angular';
+
 import {WeekMenuService} from './week-menu.service';
 import {Menu} from '../../components/menu/menu.service';
 import {Order} from '../../models/order.service';
 import {IWeekMenuState} from '../../../routes';
 import {Basket, BasketService} from '../../containers/basket/basket.service';
-import {ILogService} from 'angular';
 
 export class WeekMenuController {
-  // input bindings
+  // bindings ------------------------------------------------------------------
+  // internal
   basket: Basket;
+  actualMenu: Menu[];
+  pastDaysMenu: Menu[];
 
-  // internal bindings
-  actualMenu: Menu[] = [];
-  pastDaysMenu: Menu[] = [];
-
-  private pastDaysMenuHidden = true;
+  private pastDaysMenuHidden: boolean;
+  private loading: boolean;
 
   constructor(
     private $state: IWeekMenuState,
     private $log: ILogService,
+    private $q: IQService,
     private lWeekMenuService: WeekMenuService,
     private lBasketService: BasketService
   ) {
     'ngInject';
 
+    this.initMenu();
+    this.initPastDaysSwitcher();
+    this.initLoading();
     this.initBasket();
     this.fetchData();
   }
 
-  onOrderPlaced(order: Order) {
+  // dom event handlers --------------------------------------------------------
+  onOrderPlaced(order: Order): void {
     this.basket = this.lBasketService.addOrderTo(this.basket, order);
     const stored = this.lBasketService.storeBasketInStorage(this.basket);
 
@@ -36,16 +42,12 @@ export class WeekMenuController {
     }
   }
 
-  goToBasket() {
+  togglePastDaysMenu(): void {
+    this.pastDaysMenuHidden = !this.pastDaysMenuHidden;
+  }
+
+  goToBasket(): void {
     this.$state.go('basket');
-  }
-
-  isPastDaysMenuHidden(): boolean {
-    return this.pastDaysMenuHidden;
-  }
-
-  isPastDaysMenuShown(): boolean {
-    return !this.pastDaysMenuHidden;
   }
 
   showPastDaysMenu(): void {
@@ -56,27 +58,25 @@ export class WeekMenuController {
     this.pastDaysMenuHidden = false;
   }
 
+  // view helpers --------------------------------------------------------------
+  isLoading(): boolean {
+    return this.loading;
+  }
+
+  isPastDaysMenuHidden(): boolean {
+    return this.pastDaysMenuHidden;
+  }
+
+  isPastDaysMenuShown(): boolean {
+    return !this.pastDaysMenuHidden;
+  }
+
   hasPastDaysMenu(): boolean {
     return Boolean(this.pastDaysMenu.length);
   }
 
-  togglePastDaysMenu(): void {
-    this.pastDaysMenuHidden = !this.pastDaysMenuHidden;
-  }
-
-  private fetchData() {
-    this.lWeekMenuService.fetchPastDaysMenuForCurrentWeek()
-      .then((menus: Menu[]) => {
-        this.pastDaysMenu = menus;
-      });
-
-    this.lWeekMenuService.fetchActualMenuForCurrentWeek()
-      .then((menus: Menu[]) => {
-        this.actualMenu = menus;
-      });
-  }
-
-  private initBasket() {
+  // private init --------------------------------------------------------------
+  private initBasket(): void {
     this.lBasketService.fetchBasket()
       .then(basket => this.basket = basket)
       .catch(err => {
@@ -85,8 +85,42 @@ export class WeekMenuController {
         this.basket = new Basket();
       });
   }
+
+  private initPastDaysSwitcher(): void {
+    this.pastDaysMenuHidden = true;
+  }
+
+  private initLoading(): void {
+    this.loading = true;
+  }
+
+  private initMenu(): void {
+    this.pastDaysMenu = [];
+    this.actualMenu = [];
+  }
+
+  // private helpers -----------------------------------------------------------
+  private fetchData(): void {
+    this.loading = true;
+
+    // todo: make only one request
+    this.$q.all<Menu[]>([
+      this.lWeekMenuService.fetchPastDaysMenuForCurrentWeek(),
+      this.lWeekMenuService.fetchActualMenuForCurrentWeek()
+    ])
+      .then(menus => {
+        this.pastDaysMenu = menus[0];
+        this.actualMenu = menus[1];
+      })
+      .catch(err => this.$log.error(err))
+      .finally(() => this.loading = false);
+  }
+
+  // private event handlers ----------------------------------------------------
+  // place smth like onSmthChanged() when watching some property privately
 }
 
+// component definition --------------------------------------------------------
 export const WeekMenuComponent = {
   template: require('./week-menu.html'),
   controller: WeekMenuController,
