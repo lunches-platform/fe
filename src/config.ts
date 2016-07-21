df0619ca4c72cebfe30cace43d7cf6e3347b86aa
dezoxel <dezoxel@gmail.com>
2016-07-21 14:45:01 +0300

    LPFE-22: Added ability to specify page title in the state config

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,13 @@
 import 'moment/locale/ru';
 import * as moment from 'moment';
 type ILocalStorageServiceProvider = angular.local.storage.ILocalStorageServiceProvider;
+import {IStateService} from 'angular-ui-router';
+import {IRootScopeService} from 'angular';
+
+// todo: is it correct place?
+export interface ILRootScope extends IRootScopeService {
+  $state: IStateService;
+}
 
 export default localeConfig;
 
@@ -16,4 +23,10 @@ export function localStorageConfig(localStorageServiceProvider: ILocalStorageSer
   'ngInject';
 
   localStorageServiceProvider.setPrefix('l.');
+}
+
+export function currentStateConfig($rootScope: ILRootScope, $state: IStateService) {
+  'ngInject';
+
+  $rootScope.$state = $state;
 }
