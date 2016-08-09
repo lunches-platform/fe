060122340e44cb158e0e16f377e5a892d75fc478
dezoxel <dezoxel@gmail.com>
2016-08-09 16:02:54 +0300

    LPFE-78: Added menu type switcher to the toolbar

--- src/app/containers/week-menu/toolbar.component.ts
+++ src/app/containers/week-menu/toolbar.component.ts
@@ -1,7 +1,23 @@
+import {isNil} from 'lodash';
 import {IComponentOptions} from 'angular';
+
 import {IBaseState} from '../../../routes';
 
+import {MenuType} from '../../models/menu';
+
+// internal types --------------------------------------------------------------
+interface ITriggerMenuTypeSwitchEvent {
+  (arg: { menuType: MenuType }): void;
+}
+
 export class ToolbarController {
+  // bindings ------------------------------------------------------------------
+  // input
+  inputMenuType: string;
+
+  // output
+  triggerMenuTypeSwitchEvent: ITriggerMenuTypeSwitchEvent;
+
   constructor(private $state: IBaseState) {
     'ngInject';
   }
@@ -13,12 +29,27 @@ export class ToolbarController {
   goToMyOrders(): void {
     this.$state.go('my-orders');
   }
+
+  isSwitched(): boolean {
+    return this.inputMenuType === 'diet';
+  }
+
+  onMenuTypeSwitched(checked: boolean): void {
+    if (isNil(checked)) {
+      return;
+    }
+
+    this.triggerMenuTypeSwitchEvent({menuType: (checked ? 'diet' : 'regular')});
+  }
 }
 
 // component definition --------------------------------------------------------
 export const ToolbarComponent: IComponentOptions = {
   template: require('./toolbar.html'),
   controller: ToolbarController,
-  controllerAs: 'vm'
+  controllerAs: 'vm',
+  bindings: {
+    inputMenuType: '<selectedMenuType',
+    triggerMenuTypeSwitchEvent: '&onMenuTypeSwitched'
+  }
 };
-
