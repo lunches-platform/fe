4763d6252e04d17eccedf5e29b817a524a43d0c2
dezoxel <dezoxel@gmail.com>
2016-10-27 23:56:56 +0300

    LPFE-244: Installed ngrx/store with most basic counter example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
 
+import {counterReducer} from './counter';
 import {AppComponent} from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.provideStore({counter: counterReducer})
+  ],
+  declarations: [
+    AppComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
