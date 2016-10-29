22dc53a2444fdd4a5fb572afe20f0f11c9a71006
dezoxel <dezoxel@gmail.com>
2016-10-29 13:06:18 +0300

    LPFE-244: Simplified upgrade adapter config

--- src/app/upgrade-adapter.ts
+++ src/app/upgrade-adapter.ts
@@ -1,5 +1,4 @@
 import {UpgradeAdapter} from '@angular/upgrade';
-import {forwardRef} from '@angular/core';
 import {AppModule} from './app.module';
 
-export const upgradeAdapter = new UpgradeAdapter(forwardRef(() => AppModule));
+export const upgradeAdapter = new UpgradeAdapter(AppModule);
