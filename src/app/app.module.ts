8b4f9fc24fbacb63878dc193ddbf08e78f4eee46
dezoxel <dezoxel@gmail.com>
2016-10-31 10:36:36 +0200

    LPFE-244: Installed ngrx/store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {CounterModule} from './counter';
 import {SharedModule} from './shared';
@@ -15,6 +16,7 @@ import {AppComponent} from './app.component';
   imports: [
     BrowserModule,
     StoreModule.provideStore(reducer),
+    StoreDevtoolsModule.instrumentOnlyWithExtension(),
     SharedModule.forRoot(),
     CounterModule
   ],
